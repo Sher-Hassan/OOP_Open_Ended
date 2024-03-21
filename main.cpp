e77093a6cbc983ca0aa438e7933f4242235442f2
Shadowx69 <129595935+Shadowx69@users.noreply.github.com>
2024-03-21 10:33:10 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,6 +8,9 @@ int main() {
     Teacher teacher1("John Doe", "john.doe@example.com", "1234567890");
     Course c1("CS101", "Introduction to Computer Science", teacher1);
     Course c2("CS102", "Introduction to Computer Engineering", teacher1);
+    Course c3("CS103", "Introduction to Applied Physics", teacher1);
+    Course c4("CS104", "Object Oriented programming", teacher1);
+    Course c5("CS105", "Compiler", teacher1);
 
     Student student1("Bravo", "Bravo@gmail.com");
     Student student2("Danny", "Danny@gmail.com");
@@ -15,15 +18,17 @@ int main() {
     cout << "Student 1: " <<endl<< student1.getName() << ", " << student1.getEmail() << endl;
     student1.enrollCourse(c1);
     student1.enrollCourse(c2);
+    student1.enrollCourse(c3);
     student1.viewCourses();
 
     cout << "Student 2: " <<endl<< student2.getName() << ", " << student2.getEmail() << endl;
-    student2.enrollCourse(c1);
+    student2.enrollCourse(c4);
+    student2.enrollCourse(c5);
     student2.viewCourses();
 
-    //student1.dropCa ourse(c1.getCourseCode());
+    //student1.dropCourse(c1.getCourseCode());
 
     student2.viewCourses();
 
     return 0;
- }
+}
